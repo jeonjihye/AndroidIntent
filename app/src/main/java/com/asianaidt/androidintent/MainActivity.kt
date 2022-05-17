ec5e5a4421fdcadb15bba590d4e1f80fdb39d2e5
전지혜 <jeonjh9535@naver.com>
2022-05-17 10:20:21 +0900

    DIAL 액션예제 코드  - Uri 생성, String가공  - Intent에 다른 파라미터 대입

--- app/src/main/java/com/asianaidt/androidintent/MainActivity.kt
+++ app/src/main/java/com/asianaidt/androidintent/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.asianaidt.androidintent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDial.setOnClickListener {
+//            1. 어디에 전화를 걸지 번호 저장
+            val inputPhoneNum = edtPhoneNum.text.toString()
+
+//            2. URI로 전화연결 정보를 세팅
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+//            3. Intent를 액션 종류 + URI를 결합해서 생성
+            val myIntent = Intent(Intent.ACTION_DIAL,myUri)
+
+//            4. 실제 화면 이동
+            startActivity(myIntent)
+        }
     }
 }
